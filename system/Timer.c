430c1edd69c31643a36f0dc433a372c384f07e42
yingdemu <2458319912@qq.com>
2025-11-09 10:46:54 +0800

    Add files via upload

---
+++ system/Timer.c
@@ -0,0 +1,30 @@
+#include "stm32f10x.h" 
+#include "Encoder.h"// Device header
+
+
+void Timer_Init(void)
+{
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1,ENABLE);
+	TIM_InternalClockConfig(TIM1);
+	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
+	TIM_TimeBaseInitStruct.TIM_ClockDivision=TIM_CKD_DIV1;
+	TIM_TimeBaseInitStruct.TIM_CounterMode=TIM_CounterMode_Up;
+	TIM_TimeBaseInitStruct.TIM_Period=10000-1;
+	TIM_TimeBaseInitStruct.TIM_Prescaler=72-1;
+	TIM_TimeBaseInitStruct.TIM_RepetitionCounter=0;
+	TIM_TimeBaseInit(TIM1,&TIM_TimeBaseInitStruct);
+	
+	TIM_ITConfig(TIM1,TIM_IT_Update,ENABLE);
+	
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2);
+	
+	NVIC_InitTypeDef NVIC_InitStructure;
+	NVIC_InitStructure.NVIC_IRQChannel=TIM1_UP_IRQn;
+	NVIC_InitStructure.NVIC_IRQChannelCmd=ENABLE;
+	NVIC_InitStructure.NVIC_IRQChannelPreemptionPriority=2;
+	NVIC_InitStructure.NVIC_IRQChannelSubPriority=1;
+	NVIC_Init(&NVIC_InitStructure);
+	
+	TIM_Cmd(TIM1,ENABLE);
+}
+
